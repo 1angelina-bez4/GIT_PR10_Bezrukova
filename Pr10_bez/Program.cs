c283c116101cd90cfdf8a03fbd0dbd22e248d56d
Bezrukova <angelina_bezzrukovvva@bk.ru>
2025-11-12 21:06:24 +0300

    Adding information about yourself

--- Pr10_bez/Program.cs
+++ Pr10_bez/Program.cs
@@ -5,6 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Текущая дата:" + DateTime.Now.ToString("yyyy-MM-dd"));
+        Console.WriteLine("Безрукова Ангелина Олеговна");
+        Console.WriteLine("Ис-23Б");
     }
 }
 
